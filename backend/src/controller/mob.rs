e9021f1dbcb200bf271e87f49574554de1c67544
Luca Patarca <luca.patarca@studenti.unicam.it>
2021-06-10 20:03:42 +0200

    fix

--- backend/src/controller/mob.rs
+++
@@ -1,2 +0,0 @@
-pub mod notFound;
-pub mob sensorController;
--- backend/src/meta/sensorResponse.rs
+++
@@ -1,7 +0,0 @@
-#[derive(Serialize, Deserialize, Debug)]
-pub struct GetResponse {
-  pub _id: bson::oid::ObjectId,
-  pub email: String,
-  pub first_name: String,
-  pub last_name: String,
-}
